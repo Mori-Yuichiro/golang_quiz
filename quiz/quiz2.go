a5a7a493ac5b4f96745ed56656b81355bbe4e768
森祐一朗 <apotikisin4632@gmail.com>
2024-06-16 13:04:43 +0900

    コメントを元にquiz2.goを修正

--- quiz/quiz2.go
+++ quiz/quiz2.go
@@ -5,17 +5,10 @@ import (
 )
 
 func FindKeyByValue(m map[int]string, value string) (int, error) {
-	var err error
-	var key int
-
 	for k, v := range m {
 		if v == value {
-			key = k
-			err = nil
-			break
-		} else {
-			err = errors.New("そのvalueは存在しません")
+			return k, nil
 		}
 	}
-	return key, err
+	return 0, errors.New("そのvalueは存在しません")
 }
